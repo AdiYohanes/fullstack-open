b72e2579009c2b3cf52db512a4e901a0e7cffdc9
adiyohanes <adiyohanes19@gmail.com>
2024-08-16 14:42:57 +0700

    fullstack-finish part 2

--- part2/course/src/components/Total.jsx
+++ part2/course/src/components/Total.jsx
@@ -1,7 +1,7 @@
 const Total = ({ parts }) => {
   const total = parts.reduce((sum, part) => sum + part.exercises, 0);
 
-  return <p>Number of exercises {total}</p>;
+  return <p>Total of exercises {total}</p>;
 };
 
 export default Total;
